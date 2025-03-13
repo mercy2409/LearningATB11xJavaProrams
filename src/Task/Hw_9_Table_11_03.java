f8fe51d636e9b2ea693dffa463c1246b787e2f2f
Computer <mercy2409@gmail.com>
2025-03-13 18:21:56 +0530

    12th March work pushed on 13th March

---
+++ src/Task/Hw_9_Table_11_03.java
@@ -0,0 +1,21 @@
+package Task;
+
+public class Hw_9_Table_11_03 {
+    public static void main(String[] args) {
+        int num = 9;
+
+        System.out.println("9th table without loop");
+        System.out.printf("%d*1= %d%n" , num, (num));
+        System.out.printf("%d*2= %d%n" , num, (num*2));
+        System.out.printf("%d*3=%d%n" , num, (num*3));
+        System.out.printf("%d*4=%d%n" , num, (num*4));
+        System.out.printf("%d*5=%d%n", num, (num*5));
+        System.out.printf("%d*6=%d%n", num, (num*6));
+        System.out.printf("%d*7=%d%n", num, (num*7));
+        System.out.printf("%d*8=%d%n", num, (num*8));
+        System.out.printf("%d*9=%d%n", num, (num*9));
+        System.out.printf("%d*10=%d%n", num , (num*10));
+        System.out.printf("%d*11=%d%n" , num , (num*11));
+        System.out.printf("%d*12=%d%n" , num , (num*12));
+        }
+}
---
+++ src/Task/Hw_Math_Functions_12_03.java
@@ -0,0 +1,21 @@
+package Task;
+
+public class Hw_Math_Functions_12_03 {
+    public static void main(String[] args) {
+// The given mathematical expression is: ( 𝑥 2 + 𝑦 2 − ∣ 𝑧 ∣ ) 3 3 (x 2 +y 2 −∣z∣) ​
+        x = 10;
+        y = 10;
+        z = 10;
+        int p =2;
+        // In Java, Math.cbrt() is a built-in method used to calculate the cube root of a given number.
+        //Returns the cube root of the given number as a double.
+
+        // base: The number to be raised to a power.
+        //exponent: The power to which the base is raised.
+        //Returns: double, the result of base exponent
+
+        double result = Math.cbrt(Math.pow(x, 2)) + Math.pow(y, 2) - Math.abs(z));
+        System.out.println("Results:" + result);
+
+    }
+}
